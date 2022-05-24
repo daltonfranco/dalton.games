4aad713ba7cdde70500839130ebb618af3a64749
daltonfranco <daltonpacolafranco.git@hotmail.com>
2022-05-24 17:45:58 +0000

    alteracoes

---
+++ src/main/java/application/models/Genero.java
@@ -0,0 +1,45 @@
+package application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import java.util.Set;
+import java.util.HastSet;
+
+@Entity
+@Table(name="generos")
+
+public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    @OneToMany(mappedBy = "genero") // Um para muitos / nome do relacionamento
+    private Set<Jogo> jogos = new HastSet<> (); // Colocando coisas na classe Jogo, transformando em objeto
+
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+ 
+}
